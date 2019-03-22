7e164db8d6e9a5c0a3848f02897a2506b6dc7ed5
Gustavo Oliveira <gustavo.dev@outlook.com.br>
2019-03-22 11:10:12 -0300

    Aprendendo Iris - Testes de rotas

---
+++ 1 - Iris/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/kataras/iris"
+
+func main() {
+	app := iris.New()
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{
+			"message": "pong",
+		})
+	})
+
+	app.Get("/ping2", func(ctx iris.Context) {
+		ctx.WriteString("Testing")
+	})
+
+	app.Get("/ping3", func(ctx iris.Context) {
+		ctx.JSON(iris.Map {
+			"message": "hello world",
+			"error": "null",
+		})
+	})
+
+	// listen and serve on http://0.0.0.0:8080.
+	app.Run(iris.Addr(":8080"))
+}
