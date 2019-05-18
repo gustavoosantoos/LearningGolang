4f0d840e0449d7721c88833b026c46e0c20ab983
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-05-18 18:55:19 -0300

    Catching return and err

--- 0 - Basics/main.go
+++ 0 - Basics/main.go
@@ -7,6 +7,8 @@ import (
 func main() {
 	fmt.Println("Hello world")
 	printFoo()
+	printCatchError()
+	printIgnoringError()
 
 	for i := 0; i < 100; i++ {
 		fmt.Println(i)
@@ -20,4 +22,15 @@ func main() {
 func printFoo() {
 	fmt.Println("I'am " +
 		"a foo text")
+}
+
+func printCatchError() {
+	n, e := fmt.Println("Printing message catch possible errors")
+	fmt.Println(n)
+	fmt.Println(e)
+}
+
+func printIgnoringError() {
+	n, _ := fmt.Println("Printing message ignoring possible errors")
+	fmt.Println(n)
 }
